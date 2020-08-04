27b69fa0788a19786b5818fa80b2f05cd1fc578c
neto ferraz <netoferraz@gmail.com>
2020-08-04 09:27:35 -0300

    add cover

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # frontend_datagovprod
 Front-end do projeto de classificação de textos jurídicos
+
+
+![frontned](/imgs/frontend.png "Frontend")
--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def main():
     st.title("🧠 py-classifica-legal 🤖")
     st.subheader("Um classificador para normas infraconstitucionais ⚖️.")
     st.markdown("O *py-classifica-legal* foi treinado com uma base de mais de 30.000 normas legais. O intuito do programa é auxiliar em uma melhor governança de dados públicos, por meio de sugestões de classificações de normas infraconstitucionais a partir de suas respectivas ementas.")
+    st.markdown("Você pode consultar alguns exemplos de ementas clicando <a href='http://www4.planalto.gov.br/legislacao/portal-legis/legislacao-1/decretos1/2020-decretos' target='_blank' style='color: #f97b6f;'> aqui.</a>", unsafe_allow_html=True)
     session_state = SessionState.get(name="", button_sent=False)
     input_ementa = st.text_area("Insira o texto da ementa", "Digite aqui.")
     button_sent = st.button("Classificar")
@@ -45,7 +46,7 @@ def main():
             'PAIS ESTRANGEIRO', 'RADIODIFUSÃO', 'REFORMA AGRARIA', 'REFORÇO',
             'RENOVAÇÃO', 'SERVIÇO', 'TEXTO', 'UNIÃO FEDERAL', 'UTILIDADE PUBLICA'])
             st.warning("Não há sugestão de classificação para a ementa consultada.")
-
+    st.markdown("<center><blockquote cite='https://netoferraz.github.io/o-eu-analitico/'><p style='color: #bfc5d3;'><i>py-classifica-legal é uma prova de conceito desenvolvida por <a href='https://netoferraz.github.io/o-eu-analitico/' target='_blank' style='color: #f97b6f;'> José Ferraz Neto</a>.</i></p></blockquote></center>", unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
